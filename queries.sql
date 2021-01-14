f351f7b7259772367e2313ecea96fc6fec52e410
AtomJump <peter@atomjump.com>
2021-01-15 10:20:48 +1300

    queries for getting the last 100 negative comments and changing the password of forums

--- queries.sql
+++ queries.sql
@@ -1,10 +1,14 @@
+-- To use these queries, you must have MySQL database access to your AtomJump Messaging database (command line or some other method)
 
-
--- This will check for the last 100 agressive messages. Note: you must have the sentiment script set up and running --
+-- This will check for the last 100 agressive messages. Note: you must have the sentiment script installed in the AtomJump Server --
 select int_layer_id, var_shouted, flt_sentiment from tbl_ssshout where flt_sentiment < -4 order by int_ssshout_id desc limit 100;
 
 
--- To block a particular layer id (in this case the layer 65)
+-- You can read the full comments from a particular forum with --
+select int_layer_id, date_when_shouted, var_shouted from tbl_ssshout where int_layer_id = 65 order by int_ssshout_id desc limit 100;
+
+
+-- To block a particular forum, by changing it's password (in this case the forum 65)
 update tbl_layer set var_public_code = md5('yourpassword') where int_layer_id = 65;
 
 
